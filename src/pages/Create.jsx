70c9888bb43e7108cf6bc33b356cb05c934b73c3
clod44 <Rt_Sayochi@hotmail.com>
2024-12-13 17:31:45 +0300

    try to prevent aria warnings(failed)

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -4,6 +4,7 @@ import { MdAddPhotoAlternate } from "react-icons/md";
 import { FiSend } from "react-icons/fi";
 import { usePosts, useCloudinary } from '../hooks/useServices';
 
+//TODO: fix the stupid aria-label issue here
 const Create = () => {
     const { createPost } = usePosts();
     const { uploadImage, uploadProgress } = useCloudinary();
@@ -45,8 +46,8 @@ const Create = () => {
     };
 
     return (
-        <div className="flex flex-col gap-5 items-center justify-center p-5">
-            <div className='flex flex-col gap-2 items-center justify-center p-5 w-full sm:w-1/2'>
+        <div className="flex flex-col gap-5 items-center justify-center p-2">
+            <div className='flex flex-col gap-2 items-center justify-center p-0 w-full sm:w-1/2'>
                 {selectedFile && (
                     <>
                         <Image
@@ -54,6 +55,8 @@ const Create = () => {
                             radius='none'
                             style={{ width: "100%", height: "100%" }}
                             className='aspect-4/3'
+                            alt='Preview of selected image'
+                            aria-label="Image preview"
                         />
                         <Progress size="sm" value={uploadProgress} />
                     </>
@@ -64,6 +67,8 @@ const Create = () => {
                     fullWidth
                     value={postText}
                     onChange={(e) => setPostText(e.target.value)}
+                    aria-label='Share your thoughts'
+                    readOnly={loading}
                 />
                 <div className="flex flex-col items-center w-full gap-2">
                     <div className='flex gap-2 w-full'>
@@ -72,12 +77,16 @@ const Create = () => {
                             id="file-picker"
                             onChange={handleFileChange}
                             className="hidden"
+                            accept="image/*"
+                            aria-label='Select an image to upload'
                         />
                         <Button
                             as="label"
                             htmlFor="file-picker"
                             isIconOnly
                             className='p-2 flex-grow'
+                            aria-label="Choose an image to upload"
+                            isDisabled={loading}
                         >
                             <MdAddPhotoAlternate className='w-full h-full' />
                         </Button>
@@ -87,6 +96,7 @@ const Create = () => {
                                 className='p-2 w-full'
                                 onClick={handleSendPost}
                                 isLoading={loading}
+                                aria-label="Send post"
                                 startContent={!loading && <FiSend className='w-auto h-full aspect-square' />}
                             >
                                 Send
