23a172ff42472eb7c5800a64b337f3585f723afa
clod44 <Rt_Sayochi@hotmail.com>
2024-12-13 17:33:03 +0300

    decrease jumping and handle failure

--- src/components/PostImage.jsx
+++ src/components/PostImage.jsx
@@ -1,19 +1,47 @@
-// PostImage.js (New file)
-import { AdvancedImage, lazyload, responsive, placeholder } from '@cloudinary/react';
-import { memo } from 'react';
+import { AdvancedImage, lazyload, responsive, placeholder } from "@cloudinary/react";
+import { useState, memo } from "react";
+import { cld } from "../context/CloudinaryContext";
 
-const PostImage = memo(({ cloudinaryImage }) => (
-    <AdvancedImage
-        cldImg={cloudinaryImage}
-        width="100%"
-        height="100%"
-        radius="none"
-        plugins={[
-            lazyload({ rootMargin: '10px 20px 10px 30px', threshold: 0.25 }),
-            responsive({ steps: [800, 1000, 1400] }),
-            placeholder({ mode: 'blur' })
-        ]}
-    />
-), (prevProps, nextProps) => prevProps.cloudinaryImage === nextProps.cloudinaryImage);
+
+const fallbackImage = cld.image("notfound");
+
+const PostImage = memo(({ cloudinaryImage }) => {
+    const [imageLoaded, setImageLoaded] = useState(false);
+    const [error, setError] = useState(false);
+
+    const handleImageLoaded = () => {
+        setImageLoaded(true);
+    };
+
+    const handleImageError = () => {
+        setError(true);
+    };
+
+    return (
+        <div
+            style={{
+                width: "100%",
+                paddingTop: imageLoaded ? "0" : "100%",
+                overflow: "hidden",
+            }}
+        >
+            <AdvancedImage
+                cldImg={error ? fallbackImage : cloudinaryImage}
+                style={{
+                    width: "100%",
+                    height: "100%",
+                    objectFit: "cover",
+                }}
+                onLoad={handleImageLoaded}
+                onError={handleImageError}
+                plugins={[
+                    lazyload({ rootMargin: "10px 20px 10px 30px", threshold: 0.25 }),
+                    responsive({ steps: [800, 1000, 1400] }),
+                    placeholder({ mode: "blur" }),
+                ]}
+            />
+        </div>
+    );
+}, (prevProps, nextProps) => prevProps.cloudinaryImage === nextProps.cloudinaryImage);
 
 export default PostImage;
