ef9bb192ded9209e438feafa064a24113e9519bc
clod44 <Rt_Sayochi@hotmail.com>
2024-12-14 10:58:07 +0300

    pull to refresh

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,34 +1,45 @@
 import { useEffect, useState } from 'react';
+import { Spinner } from '@nextui-org/react';
+import PullToRefresh from 'react-simple-pull-to-refresh';
 import Post from '../components/Post';
 import { usePosts } from '../hooks/useServices';
-import ReactPullToRefresh from 'react-pull-to-refresh';
 const Home = () => {
     const { getAllPosts } = usePosts();
     const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(false);
     const fetchPosts = async () => {
+        setLoading(true);
         try {
+            await new Promise((resolve) => setTimeout(resolve, 1000));
             const posts = await getAllPosts();
             console.log("posts", posts);
             setPosts(posts);
         } catch (error) {
             console.error('Error fetching posts:', error);
             setPosts([]);
+        } finally {
+            setLoading(false);
         }
     };
     useEffect(() => {
         fetchPosts();
     }, [])
-    const handleRefresh = () => {
-        fetchPosts();
+    const handleRefresh = async () => {
+        await fetchPosts();
     };
 
     return (
-        <div className='grid grid-cols-1 items-center p-2 py-5 gap-5'>
-            <ReactPullToRefresh onRefresh={handleRefresh} className="" style={{ textAlign: 'center' }}>
+        <div className='grid grid-cols-1 items-center p-2 pb-5 gap-5'>
+
+            <PullToRefresh
+                onRefresh={handleRefresh}
+                refreshingContent={<Spinner color="default" className="w-8 h-8 mt-4 animate-spin" />}
+                pullingContent={""}
+            >
                 {posts.map((post, index) => (
                     <Post key={index} post={post} />
                 ))}
-            </ReactPullToRefresh>
+            </PullToRefresh>
         </div>
     );
 }
